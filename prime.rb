5c4ee0217c465fb907a88e1ec24975975e02ac3d
Martinique <me@martidolce.com>
2020-11-14 13:48:47 -0500

    commit tree 7d5324e7a25b69024c2f15d99630dfca2e01b9f1

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+# frozen_string_literal: true
+
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+def prime?(num)
+  if !(num.negative? || num.zero? || (num == 1))
+    (2..num - 1).to_a.all? do |i|
+      num % i != 0
+    end
+  else
+    false
+  end
+end
